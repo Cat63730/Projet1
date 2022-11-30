3ad273ab663cfa7eb0a9fcb5c135103f1905960e
Cat63730 <94469220+Cat63730@users.noreply.github.com>
2022-11-30 11:40:27 +0100

    add index2.html

--- main.js
+++ main.js
@@ -1,44 +1,12 @@
-class carousel {
+// const 
+// const item2 = document.querySelector(".item2");
 
-/**
- * 
- * @param {HTMLElement} element 
- * @param {*} options
- * @param {Object} options.slidesToScroll Nombre d'elements à faire défiler
- * @param {object} options.slidesVisible Nombre d'elements visibles dans un slide
- */
-
-    constructor (element, options = {}){
-        this.element =element
-        this.options = object.assign({},{
-            slidesToScroll: 1,
-            slidesVisible: 1
-        }, options)
-        this.children = [].slice.call(element.children)
-        let root = this.createDivWithClass('carousel')
-        let container = this.createDivWithClass('carousel__container')
-        root.appendChild(container)
-        this.element.appendChild(root)
-    }
-
-    createDivWithClass (className) {
-        let div = document.createElement('div')
-        div.setAttribute('class', className)
-        return div
-    }
-
-}
-
-/**
- * 
- * @param {string} className
- * @returns {HTMLElement}
- */
-
-document.addEventListener('DOMContentLoaded', Function() {
-    new carousel(document.querySelector('#carousel1'), {
-        slidesToScroll: 3,
-        slidesVisible: 3
-    }) 
-})
-
+// const imgCaroussel = [
+//     {
+//         galerie: "Corps d'emprunt",
+//         picture: "url(assets/BackstageDesktop.jpg)"
+//     }
+//     {
+//         galerie: "Hero"
+//     }
+// ]
