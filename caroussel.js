8234528ebd3196abb42215780cf01fd740392120
Cat63730 <94469220+Cat63730@users.noreply.github.com>
2022-12-09 10:31:48 +0100

    menu burger on corpsemprunt.html

--- caroussel.js
+++ caroussel.js
@@ -64,21 +64,21 @@ const bigImg = document.querySelector(".modal-content img");
 for (let i = 0; i < smallsImgs.length; i++) {
   smallsImgs[i].addEventListener("click", () => {
     let smallImgSrc = smallsImgs[i].src.split("/").pop();
-    bigImg.src = `./assets/copy_${smallImgSrc}`;
+    bigImg.src = `./assets/EntierEmprunt/${smallImgSrc}`;
     modal.style.display = "block";
   });
 }
 
-// smallsImgs = [...smallsImgs];
+smallsImgs = [...smallsImgs];
 
-// smallsImgs.map((smallImg) => {
-//   smallImg.addEventListener("click", () => {
-//     let smallImgSrc = smallImg.src.split("/").pop();
-//     console.log(smallImgSrc);
-//     bigImg.src = `./assets/copy_${smallImgSrc}`;
-//     modal.style.display = "block";
-//   });
-// });
+smallsImgs.map((smallImg) => {
+  smallImg.addEventListener("click", () => {
+    let smallImgSrc = smallImg.src.split("/").pop();
+    console.log(smallImgSrc);
+    bigImg.src = `./assets/EntierEmprunt/${smallImgSrc}`;
+    modal.style.display = "block";
+  });
+});
 
 modal.addEventListener("click", () => {
   modal.style.display = "none";
